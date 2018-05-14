1f89e9ee0dc83d890a6a886b82df4e99cc0b9203
ashwinmr <iashwinrao@gmail.com>
2018-05-13 20:53:13 -0400

    completed design

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const url = require('url')
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({ title: " ",icon:'./assets/icons/main.png', resizable: false, width: 400, height: 125 })
 
     // and load the index.html of the app.
     win.loadURL(url.format({
@@ -12,6 +12,9 @@ function createWindow() {
         protocol: 'file:',
         slashes: true
     }))
+
+    // Remove menu bar
+    win.setMenu(null)
 
     // Open the DevTools.
     // win.webContents.openDevTools()
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const path = require('path')
 const url = require('url')
 
-const Timer_Set = 5
+var Reset_Val = 10
 var Minutes = 0
 var Seconds = 0
 var Minutes_Elem = document.getElementById("minutes")
@@ -11,54 +11,77 @@ var Play_Pause_Button = document.getElementById("play_pause_button")
 var Reset_Button = document.getElementById("reset_button")
 var Paused = true
 
-
-function Reset() {
-    Minutes = Timer_Set
-    Seconds = 0
-    Minutes_Elem.value = Minutes
-    Seconds_Elem.value = Seconds
-}
-
-function End() {
-    Paused = true
-}
-
-function Update() {
-    if (!Paused) {
-        Seconds -= 1
-        if (Seconds < 0) {
-            Seconds = 59
-            Minutes -= 1
-            if (Minutes < 0) {
-                Seconds = 0
-                Minutes = 0
-                End()
-            }
-        }
-        Minutes_Elem.value = Minutes
-        Seconds_Elem.value = Seconds
+/* Function to set minutes with 2 digits */
+function Set_Minutes(minutes){
+    minutes = parseInt(minutes)
+    Minutes = minutes
+    if(minutes < 10){
+        Minutes_Elem.value = '0' + minutes;
+    }
+    else{
+        Minutes_Elem.value = minutes;
     }
 }
 
-function Start() {
-    Minutes = Minutes_Elem.value
-    Seconds = Seconds_Elem.value
+/* FUnction to set seconds with 2 digits */
+function Set_Seconds(seconds){
+    seconds = parseInt(seconds)
+    Seconds = seconds
+    if(seconds < 10){
+        Seconds_Elem.value = '0' + seconds;
+    }
+    else{
+        Seconds_Elem.value = seconds;
+    }
+}
+
+function Reset() {
+    Set_Minutes(Reset_Val)
+    Set_Seconds(0)
+}
+
+function End() {
+    Play_Pause()
+}
+
+function Update() {
+    minutes = Minutes
+    seconds = Seconds
+    if (!Paused) {
+        seconds -= 1
+        if (seconds < 0) {
+            seconds = 59
+            minutes -= 1
+            if (minutes < 0) {
+                seconds = 0
+                minutes = 0
+                End()
+            }
+        }
+        Set_Minutes(minutes)
+        Set_Seconds(seconds)
+    }
 }
 
 function Play_Pause() {
     if (Paused) {
         Paused = false
-        Play_Pause_Button.style.backgroundImage = 'url(../assets/images/pause.png)'
-        Start()
+        Play_Pause_Button.style.setProperty('-webkit-mask-image','url(../assets/images/pause.png)')
+        Set_Minutes(Minutes_Elem.value)
+        Set_Seconds(Seconds_Elem.value)
     }
     else {
         Paused = true
-        Play_Pause_Button.style.backgroundImage = 'url(../assets/images/play.png)'
+        Play_Pause_Button.style.setProperty('-webkit-mask-image','url(../assets/images/play.png)')
     }
 }
 
 Play_Pause_Button.addEventListener('click', Play_Pause)
 Reset_Button.addEventListener('click',Reset)
+Minutes_Elem.addEventListener('change',()=>{Set_Minutes(Minutes_Elem.value)
+    Reset_Val = Minutes
+})
+Seconds_Elem.addEventListener('change',()=>{Set_Seconds(Seconds_Elem.value)})
 
 Reset()
 setInterval(Update, 1000)
